cb33671198ed9a98cbfe6aba0fa984a80578f22c
nikulawx <11022020ua1@gmail.com>
2025-01-15 19:29:43 +0200

    cart

---
+++ script.js
@@ -0,0 +1,17 @@
+let products = [
+    {
+        "title":"Apple iPhone 16 pro",
+        "price": 65 000,
+        "image":"https://content2.rozetka.com.ua/goods/images/big/468886490.jpg",
+        "description":"Новітній контролер камери. Такий швидкий. Такий плавний.Тонші рамки для більших дисплеїв.Прорив у часі роботи від акумулятора."
+    }
+    function getCookieValue(cookieName) {
+        const cookies = document.cookie.split(';');
+
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            
+            if (cookie)
+        }
+    }
+]
